c7267d95482534cc39896e6d164e4f5817d87173
brentroberts989 <prog@chaos.local>
2019-05-23 21:05:27 -0400

    Added pneumatics

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -6,13 +6,17 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.ADXRS450_Gyro;
 import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.DriverStation;
 
 public class Robot extends TimedRobot {
   public static final Spark INTAKEMOT = new Spark(Constants.INTAKE_1);
   private Xbox driver = new Xbox(0);
-	private DriveTrain dt = new DriveTrain();
+  private DriveTrain dt = new DriveTrain();
+  private Compressor compressor;
+  private Solenoid in;
+  private Solenoid out;
   private static final String kDefaultAuto = "Default";
   private static final String kCustomAuto = "My Auto";
   private String m_autoSelected;
@@ -24,6 +28,9 @@ public class Robot extends TimedRobot {
     m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
     m_chooser.addOption("My Auto", kCustomAuto);
     SmartDashboard.putData("Auto choices", m_chooser);
+    compressor = new Compressor();
+    in = new Solenoid(1);
+    out = new Solenoid(0);
   }
 
  
@@ -55,6 +62,8 @@ public class Robot extends TimedRobot {
   @Override
   public void teleopPeriodic() {
 
+    compressor.setClosedLoopControl(true);
+
     double forward = 0;
 		double turn = 0;
 		forward = driver.getY(GenericHID.Hand.kLeft);
@@ -62,16 +71,26 @@ public class Robot extends TimedRobot {
     
     dt.accelDrive(-forward, -turn);
 
-    if (driver.when(Xbox.buttons.a)) {
+    if (driver.getPressed(Xbox.buttons.a)) {
       Common.dashNum("intake power", 1);
       INTAKEMOT.set(1); 
     }
-    else if (driver.when(Xbox.buttons.b)) {
+    else if (driver.getPressed(Xbox.buttons.b)) {
       INTAKEMOT.set(-1);
       Common.dashNum("intake power", -1);
     }
     else{
       INTAKEMOT.set(0);
+    }
+
+    if (driver.when(Xbox.buttons.y)) {
+      //open
+      out.set(true);
+      in.set(false);
+    } else if (driver.when(Xbox.buttons.x)) {
+      //close
+      out.set(false);
+      in.set(true);
     }
       
   }
